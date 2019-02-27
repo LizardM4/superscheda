dd1e5f25351a76bba4b6429df1e1ffd3e616f685
Pietro Saccardi <lizardm4@gmail.com>
2019-02-27 21:23:05 +0100

    Moving function to graph

--- js/ddgraph.js
+++ js/ddgraph.js
@@ -6,253 +6,18 @@ const DDType = Object.freeze({
     NONE:    Symbol('none')
 });
 
-class DDNode {
-
-    get obj() {
-        return this._$obj;
+class DDGraph {
+    get root() {
+        return this._root;
     }
 
-    get parent() {
-        return this._parent;
-    }
-
-    get path() {
-        return this._path;
-    }
-
-    get children() {
-        return this._children;
-    }
-
-    get id() {
-        return this._id;
-    }
-
-    get baseId() {
-        return this._baseId;
-    }
-
-    get indices() {
-        return this._indices;
-    }
-
-    get type() {
-        return this._type;
-    }
-
-    get isVoid() {
-        return testVoid(this._getRawValue());
-    }
-
-    get value() {
-        return castRawValue(this.type, this._getRawValue());
-    }
-
-    set value(v) {
-        this._setRawValue(formatValue(this.type, v));
-    }
-
-    get isRoot() {
-        return !this.parent;
-    }
-
-    get formulaValue() {
-        if (this.isVoid) {
-            return this._formulaValue;
-        }
-        return castRawValue(this.type, this._getRawValue(), true);
-    }
-
-    set formulaValue(v) {
-        this._formulaValue = v;
-        this._updateFormulaValue();
-    }
-
-    constructor($obj, parent=null) {
-        if (typeof $obj === 'undefined') {
-            // We are creating a root
-            console.assert(typeof parent === 'undefined' || parent == null);
-            parent = null
-            $obj = null;
-        }
-        this._$obj = $obj;
-        this._parent = parent;
-        this._children = [];
-        this._childById = {};
-        this._id = null;
-        this._indices = null;
-        this._baseId = null;
-        this._path = null;
-        this._idx = null;
-        this._isCheckbox = false;
-        this._holdsData = false;
-        this._formulaValue = null;
-        this._type = DDType.NONE;
+    constructor() {
+        this._root = new DDNode(this);
         this._descendantsByPath = {};
         this._leavesByPath = {};
-        if (!this.isRoot) {
-            this._setup();
-        }
-    }
-
-    hasChild(child) {
-        console.assert(!this.holdsData);
-        return this._childById[child.id] === child;
-    }
-
-    _updateChild(updatedChild) {
-        console.assert(!this.holdsData);
-        updateDict(this._childById, updatedChild, updatedChild.id);
-        this.getRoot()._updateDescendant(updatedChild);
-    }
-
-    _updateDescendant(updatedDescendant) {
-        console.assert(this.isRoot);
-        updateDict(this._descendantsByPath, updatedDescendant, updatedDescendant.path);
-        if (updatedDescendant.holdsData) {
-            updateDict(this._leavesByPath, updatedDescendant, updatedDescendant.path);
-        }
-    }
-
-    _addDescendant(descendant) {
-        console.assert(this.isRoot);
-        this._descendantsByPath[descendant.path] = descendant;
-        if (descendant.holdsData) {
-            this._leavesByPath[descendant.path] = descendant;
-        }
-    }
-
-    _removeDescendant(descendant) {
-        console.assert(this.isRoot);
-        delete this._descendantsByPath[descendant.path];
-        if (descendant.holdsData) {
-            delete this._leavesByPath[descendant.path];
-        }
-    }
-
-    getRoot() {
-        if (!this.isRoot) {
-            return this.parent.getRoot();
-        }
-        return this;
-    }
-
-    _addChild(child) {
-        console.assert(!this.holdsData);
-        console.assert(!(child.id in this._childById));
-        this._children.push(child);
-        this._childById[child.id] = child;
-        this.getRoot()._addDescendant(child);
-    }
-
-    _removeChild(child) {
-        console.assert(!this.holdsData);
-        console.assert(this.hasChild(child));
-        this.getRoot()._removeDescendant(child);
-        delete this._childById[child.id];
-        const idx = this._children.indexOf(child);
-        console.assert(idx >= 0);
-        this._children.splice(idx, 1);
-    }
-
-    _updateFormulaValue() {
-        console.assert(this._holdsData);
-        this.obj.attr('placeholder', formatValue(this.type, this._formulaValue));
-    }
-
-    _getRawValue() {
-        console.assert(this._holdsData);
-        if (this._isCheckbox) {
-            return this.obj.is(':checked');
-        }
-        const val = this.obj.val();
-        if (typeof val === 'undefined' || val == null || val == '') {
-            return null;
-        }
-        return val;
-    }
-
-    _setRawValue(v) {
-        console.assert(this._holdsData);
-        if (this._isCheckbox) {
-            console.assert(typeof v === 'boolean');
-            this.obj.prop('checked', v);
-        } else {
-            console.assert(typeof v === 'string');
-            this.obj.val(v);
-        }
-    }
-
-    _getIndices() {
-        console.assert(!this.isRoot);
-        // Search for all data-dd-array="item" between this object and the parent
-        const filter = '[data-dd-array="item"]';
-        const ddItems = this.obj.parentsUntil(this.parent.obj, filter);
-        if (ddItems.length == 0) {
-            return null;
-        } else if (ddItems.length == 1) {
-            return parseInt(ddItems.attr('data-dd-index'));
-        } else {
-            return ddItems.map((i, item) => parseInt($(item).attr('data-dd-index')));
-        }
-    }
-
-    _setup() {
-        console.assert(!this.isRoot);
-        console.assert(this.obj);
-        this._baseId = this.obj.attr('data-dd-id');
-        this._isCheckbox = (this.obj.attr('type') === 'checkbox');
-        this._holdsData = holdsData(this.obj);
-        this._type = inferType(this.obj);
-        // TODO infer formula
-        this.obj.data('ddNode') = this;
-        // Mutable properties:
-        this._indices = this._getIndices(this.parent);
-        this._setupIdAndPath();
-        if (this.parent) {
-            this.parent.addChild(this);
-        }
-    }
-
-    _setupIdAndPath() {
-        console.assert(!this.isRoot);
-        this._id = this.baseId + indicesToString(this.indices);
-        this._path = combinePath(this.parent.path, this.id);
-    }
-
-    reindexIfNeeded() {
-        console.assert(!this.isRoot);
-        const oldIndices = this.indices;
-        const newIndices = this._getIndices();
-        if (oldIndices != newIndices) {
-            this._indices = newIndices;
-            this._setupIdAndPath();
-            if (this.parent) {
-                this.parent._updateChild(this);
-            } else {
-                this.getRoot()._updateDescendant(this);
-            }
-        }
-    }
-
-    childById(id) {
-        const child = this._childById[id];
-        if (typeof child === 'undefined') {
-            return null;
-        }
-        return child;
-    }
-
-    childrenById(ids, filterMissing=true) {
-        const children = ids.map(id => this.childById(id));
-        if (filterMissing) {
-            return children.filter(child => typeof child !== 'undefined');
-        }
-        return children;
     }
 
     descendantByPath(path) {
-        console.assert(this.isRoot);
         const descendant = this._descendantsByPath[id];
         if (typeof descendant === 'undefined') {
             return null;
@@ -261,7 +26,6 @@ class DDNode {
     }
 
     leafByPath(path) {
-        console.assert(this.isRoot);
         const leaf = this._leavesByPath[id];
         if (typeof leaf === 'undefined') {
             return null;
@@ -269,6 +33,26 @@ class DDNode {
         return leaf;
     }
 
+    _updateDescendant(updatedDescendant) {
+        updateDict(this._descendantsByPath, updatedDescendant, updatedDescendant.path);
+        if (updatedDescendant.holdsData) {
+            updateDict(this._leavesByPath, updatedDescendant, updatedDescendant.path);
+        }
+    }
+
+    _addDescendant(descendant) {
+        this._descendantsByPath[descendant.path] = descendant;
+        if (descendant.holdsData) {
+            this._leavesByPath[descendant.path] = descendant;
+        }
+    }
+
+    _removeDescendant(descendant) {
+        delete this._descendantsByPath[descendant.path];
+        if (descendant.holdsData) {
+            delete this._leavesByPath[descendant.path];
+        }
+    }
     static indicesToString(indices) {
         if (typeof indices === 'number') {
             return '[' + indices.toString() + ']';
@@ -394,4 +178,216 @@ class DDNode {
         }
         return value.toString();
     }
+}
+
+class DDNode {
+
+    get obj() {
+        return this._$obj;
+    }
+
+    get parent() {
+        return this._parent;
+    }
+
+    get path() {
+        return this._path;
+    }
+
+    get children() {
+        return this._children;
+    }
+
+    get id() {
+        return this._id;
+    }
+
+    get baseId() {
+        return this._baseId;
+    }
+
+    get indices() {
+        return this._indices;
+    }
+
+    get type() {
+        return this._type;
+    }
+
+    get isVoid() {
+        return DDGraph.testVoid(this._getRawValue());
+    }
+
+    get value() {
+        return DDGraph.castRawValue(this.type, this._getRawValue());
+    }
+
+    set value(v) {
+        this._setRawValue(DDGraph.formatValue(this.type, v));
+    }
+
+    get isRoot() {
+        return !this.parent;
+
+    get graph() {
+        return this._graph;
+    }
+
+    get formulaValue() {
+        if (this.isVoid) {
+            return this._formulaValue;
+        }
+        return DDGraph.castRawValue(this.type, this._getRawValue(), true);
+    }
+
+    set formulaValue(v) {
+        this._formulaValue = v;
+        this._updateFormulaValue();
+    }
+
+    constructor(graph, $obj, parent=null) {
+        this._graph = graph;
+        if (typeof $obj === 'undefined') {
+            // We are creating a root
+            console.assert(typeof parent === 'undefined' || parent == null);
+            parent = null
+            $obj = null;
+        }
+        this._$obj = $obj;
+        this._parent = parent;
+        this._children = [];
+        this._childById = {};
+        this._id = null;
+        this._indices = null;
+        this._baseId = null;
+        this._path = null;
+        this._idx = null;
+        this._isCheckbox = false;
+        this._holdsData = false;
+        this._formulaValue = null;
+        this._type = DDType.NONE;
+        if (!this.isRoot) {
+            this._setup();
+        }
+    }
+
+    hasChild(child) {
+        console.assert(!this.holdsData);
+        return this._childById[child.id] === child;
+    }
+
+    _updateChild(updatedChild) {
+        console.assert(!this.holdsData);
+        DDGraph.updateDict(this._childById, updatedChild, updatedChild.id);
+    }
+
+    _addChild(child) {
+        console.assert(!this.holdsData);
+        console.assert(!(child.id in this._childById));
+        this._children.push(child);
+        this._childById[child.id] = child;
+    }
+
+    _removeChild(child) {
+        console.assert(!this.holdsData);
+        console.assert(this.hasChild(child));
+        delete this._childById[child.id];
+        const idx = this._children.indexOf(child);
+        console.assert(idx >= 0);
+        this._children.splice(idx, 1);
+    }
+
+    _updateFormulaValue() {
+        console.assert(this._holdsData);
+        this.obj.attr('placeholder', DDGraph.formatValue(this.type, this._formulaValue));
+    }
+
+    _getRawValue() {
+        console.assert(this._holdsData);
+        if (this._isCheckbox) {
+            return this.obj.is(':checked');
+        }
+        const val = this.obj.val();
+        if (typeof val === 'undefined' || val == null || val == '') {
+            return null;
+        }
+        return val;
+    }
+
+    _setRawValue(v) {
+        console.assert(this._holdsData);
+        if (this._isCheckbox) {
+            console.assert(typeof v === 'boolean');
+            this.obj.prop('checked', v);
+        } else {
+            console.assert(typeof v === 'string');
+            this.obj.val(v);
+        }
+    }
+
+    _getIndices() {
+        console.assert(!this.isRoot);
+        // Search for all data-dd-array="item" between this object and the parent
+        const filter = '[data-dd-array="item"]';
+        const ddItems = this.obj.parentsUntil(this.parent.obj, filter);
+        if (ddItems.length == 0) {
+            return null;
+        } else if (ddItems.length == 1) {
+            return parseInt(ddItems.attr('data-dd-index'));
+        } else {
+            return ddItems.map((i, item) => parseInt($(item).attr('data-dd-index')));
+        }
+    }
+
+    _setup() {
+        console.assert(!this.isRoot);
+        console.assert(this.obj);
+        this._baseId = this.obj.attr('data-dd-id');
+        this._isCheckbox = (this.obj.attr('type') === 'checkbox');
+        this._holdsData = DDGraph.holdsData(this.obj);
+        this._type = DDGraph.inferType(this.obj);
+        // TODO infer formula
+        this.obj.data('ddNode') = this;
+        // Mutable properties:
+        this._indices = this._getIndices(this.parent);
+        this._setupIdAndPath();
+        if (this.parent) {
+            this.parent.addChild(this);
+        }
+    }
+
+    _setupIdAndPath() {
+        console.assert(!this.isRoot);
+        this._id = this.baseId + DDGraph.indicesToString(this.indices);
+        this._path = DDGraph.combinePath(this.parent.path, this.id);
+    }
+
+    reindexIfNeeded() {
+        console.assert(!this.isRoot);
+        const oldIndices = this.indices;
+        const newIndices = this._getIndices();
+        if (oldIndices != newIndices) {
+            this._indices = newIndices;
+            this._setupIdAndPath();
+            this.parent._updateChild(this);
+            // TODO update root
+        }
+    }
+
+    childById(id) {
+        const child = this._childById[id];
+        if (typeof child === 'undefined') {
+            return null;
+        }
+        return child;
+    }
+
+    childrenById(ids, filterMissing=true) {
+        const children = ids.map(id => this.childById(id));
+        if (filterMissing) {
+            return children.filter(child => typeof child !== 'undefined');
+        }
+        return children;
+    }
+
 }
