ea27b589252cdaf39a219d0b27e422e90d653fdd
Pietro Saccardi <lizardm4@gmail.com>
2019-04-17 18:24:05 +0200

    Loading polyfills and other stuff lazily

--- src/index.js
+++ src/index.js
@@ -1,11 +1,30 @@
+// Shim Promises on older browsers
+import 'es6-promise/auto';
+
+
+// Import fetch for Dropbox to use
+import(/* webpackChunkName: "fetch" */ 'isomorphic-fetch').then(fetch => {
+    console.log('fetch', fetch);
+});
+
+// Lazily import dropbox
+import(/* webpackChunkName: "dropbox" */ 'dropbox').then(Dropbox => {
+    console.log('Dropbox', Dropbox);
+});
+
+// Load the controller
+import(/* webpackChunkName: "controller" */ './js/controller').then(controller => {
+    console.log('controller', controller);
+});
+
+
+// TODO: construct the controller with dropbox
+
+
 import './css/damagetype.css';
 import './css/forkme.css';
 import './css/bootstrap.scss';
 
-import 'bootstrap';
-import { Sortable } from 'sortablejs';
-
-import(/* webpackChunkName: "controller" */ './js/controller').then(controller => console.log(controller));
 
 
 import { library, dom } from '@fortawesome/fontawesome-svg-core';
@@ -18,3 +37,7 @@ console.log('Hello webpack');
 const sth = (a) => 2 * a;
 
 console.log(sth(22));
+
+import 'bootstrap';
+import { Sortable } from 'sortablejs';
+
