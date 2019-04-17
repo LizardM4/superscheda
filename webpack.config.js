7515c2167a892fd740d201994c5fe98fb265bab7
Pietro Saccardi <lizardm4@gmail.com>
2019-04-17 18:24:04 +0200

    Added more development tools

--- webpack.config.js
+++ webpack.config.js
@@ -9,6 +9,10 @@ module.exports = {
     filename: 'bundle.js',
     path: path.resolve(__dirname, 'dist')
   },
+  devtool: 'inline-source-map',
+  devServer: {
+    contentBase: './dist'
+  },
   plugins: [
     new CleanWebpackPlugin(),
     new HtmlWebpackPlugin({
