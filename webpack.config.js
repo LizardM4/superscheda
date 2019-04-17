885b29b35f7ddeb6d5caf9937df1327b15f20ece
Pietro Saccardi <lizardm4@gmail.com>
2019-04-17 18:24:04 +0200

    Added file loader for fonts and pics

--- webpack.config.js
+++ webpack.config.js
@@ -11,10 +11,15 @@ module.exports = {
     rules: [
       {
         test: /\.css$/,
-        use: [
-          'style-loader',
-          'css-loader'
-        ]
+        use: ['style-loader', 'css-loader']
+      },
+      {
+        test: /\.(woff|woff2|eot|ttf|otf)$/,
+        use: ['file-loader']
+      },
+      {
+        test: /\.(svg|mp4|webm)$/,
+        use: ['file-loader']
       }
     ]
   }
