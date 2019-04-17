cd38f2acc0447968589cab80be380f4e54e4e18b
Pietro Saccardi <lizardm4@gmail.com>
2019-04-17 18:24:05 +0200

    Added SortableJS

--- src/js/index.js
+++ src/js/index.js
@@ -1,7 +1,9 @@
 import '../css/damagetype.css';
 import '../css/forkme.css';
-import 'bootstrap';
 import '../css/bootstrap.scss';
+
+import 'bootstrap';
+import { Sortable } from 'sortablejs';
 
 import { library, dom } from '@fortawesome/fontawesome-svg-core';
 import { faCamera } from '@fortawesome/free-solid-svg-icons';
