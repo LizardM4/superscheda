f9531b90c0f674fb976c39a7904047f052810b0c
Pietro Saccardi <lizardm4@gmail.com>
2019-04-17 18:24:05 +0200

    Bunding fontawesome in

--- src/js/index.js
+++ src/js/index.js
@@ -2,4 +2,10 @@ import '../css/damagetype.css';
 import '../css/forkme.css';
 import 'bootstrap';
 import '../css/bootstrap.scss';
+
+import { library, dom } from '@fortawesome/fontawesome-svg-core';
+import { faCamera } from '@fortawesome/free-solid-svg-icons';
+library.add(faCamera);
+dom.watch();
+
 console.log('Hello webpack');
